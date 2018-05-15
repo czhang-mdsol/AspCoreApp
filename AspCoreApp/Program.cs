5104d97ce216debab7f73b018ceb035a823c9f45
Chunrong Zhang <czhang@mdsol.com>
2018-05-15 15:58:45 +0900

    Fix code issue

--- AspCoreApp/Program.cs
+++ AspCoreApp/Program.cs
@@ -15,7 +15,6 @@ namespace AspCoreApp
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-            console
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
